ed31d7c621bb39f005ce75b42624d76c20cdf805
Daniel Sandoval <ddanidoble@gmail.com>
2024-08-11 10:17:14 -0600

    minor fixes

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,7 @@ export default defineConfig({
         lib: {
             // Could also be a dictionary or array of multiple entry points
             entry: resolve(_dirname, 'lib/main.js'),
-            name: 'Webserial',
+            name: 'WebSerial',
             // the proper extensions will be added
             fileName: 'webserial',
         },
