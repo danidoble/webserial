726d64b2794f3d9f3a1ebe3893676b9a95557f37
danidoble <ddanidoble@gmail.com>
2024-08-16 17:57:27 -0600

    fixing dispense locked elevator

---
+++ test/connection.jofemar.js
@@ -0,0 +1,32 @@
+import './style.css';
+import {Jofemar} from './../lib/serial/jofemar.js';
+
+const jofemar = new Jofemar();
+jofemar.on('serial:message', data => {
+    console.log(data);
+});
+// jofemar.on('serial:disconnected', () => {
+//     console.log('serial device was disconnected');
+// });
+// jofemar.on('dispensed', () => {
+//     console.log('Cell opened');
+// });
+// jofemar.on('not-dispensed', () => {
+//     console.log('Cell not opened');
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
+    tryConnect();
+
+    const connect = document.getElementById('testConnection');
+    connect.addEventListener('click', async () => {
+        await jofemar.connect().catch(console.error);
+    });
+});
+
+function tryConnect() {
+    jofemar.connect().then(() => {
+    }).catch(console.error);
+}
+
+window.jofemar = jofemar;
