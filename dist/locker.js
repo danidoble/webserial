6e89cc800ae4d88409dfa1870e2b770f9e7dceb2
danidoble <ddanidoble@gmail.com>
2025-06-23 14:46:37 -0600

    fix timeout on dispense

--- dist/locker.js
+++ dist/locker.js
@@ -5,17 +5,17 @@ var m = (l, c, e) => c.has(l) || w("Cannot " + e);
 var r = (l, c, e) => (m(l, c, "read from private field"), e ? e.call(l) : c.get(l)), u = (l, c, e) => c.has(l) ? w("Cannot add the same private member more than once") : c instanceof WeakSet ? c.add(l) : c.set(l, e), i = (l, c, e, s) => (m(l, c, "write to private field"), s ? s.call(l, e) : c.set(l, e), e), n = (l, c, e) => (m(l, c, "access private method"), e);
 import { K as A, h as v } from "./kernel-BLbpgXHp.js";
 import { u as p } from "./relay-Dop4mFk-.js";
-var h, a, o, t, y, d, f, g, b, C, T;
+var h, a, o, t, y, _, f, g, b, C, T;
 class M extends A {
-  constructor({ filters: e = null, config_port: s = null, no_device: _ = 1, device_listen_on_port: k = 3 } = {}) {
-    super({ filters: e, config_port: s, no_device: _, device_listen_on_port: k });
+  constructor({ filters: e = null, config_port: s = null, no_device: d = 1, device_listen_on_port: k = 3 } = {}) {
+    super({ filters: e, config_port: s, no_device: d, device_listen_on_port: k });
     u(this, t);
     u(this, h, !1);
     u(this, a, 0);
     u(this, o, 0);
-    if (this.__internal__.device.type = "locker", v.getCustom(this.typeDevice, _))
-      throw new Error(`Device ${this.typeDevice} ${_} already exists`);
-    this.__internal__.device.milliseconds = 666, this.__internal__.dispense.limit_counter = 1, v.add(this), n(this, t, y).call(this);
+    if (this.__internal__.device.type = "locker", v.getCustom(this.typeDevice, d))
+      throw new Error(`Device ${this.typeDevice} ${d} already exists`);
+    this.__internal__.time.response_engines = 1e3, this.__internal__.device.milliseconds = 666, this.__internal__.dispense.limit_counter = 1, v.add(this), n(this, t, y).call(this);
   }
   serialMessage(e) {
     const s = {
@@ -59,7 +59,7 @@ class M extends A {
   async dispense({ cell: e = 1, status: s = !0 } = {}) {
     return setTimeout(() => {
       s === !0 ? n(this, t, C).call(this) : n(this, t, T).call(this);
-    }, 10), await this.internalDispense(
+    }, this.__internal__.time.response_engines / 2), await this.internalDispense(
       p.openCell({
         cell: e,
         channel: this.__internal__.device.listen_on_port
@@ -105,32 +105,32 @@ class M extends A {
   }
   async openAll() {
     if (this.isDispensing) throw new Error("Another dispensing process is running");
-    n(this, t, d).call(this), i(this, h, !0), n(this, t, f).call(this);
+    n(this, t, _).call(this), i(this, h, !0), n(this, t, f).call(this);
     const e = [];
     for (let s = 1; s <= 90; s++) {
-      const _ = await this.dispense(s);
-      e.push(_), i(this, a, s), n(this, t, f).call(this);
+      const d = await this.dispense(s);
+      e.push(d), i(this, a, s), n(this, t, f).call(this);
     }
-    i(this, a, 90), n(this, t, f).call(this, e), n(this, t, d).call(this);
+    i(this, a, 90), n(this, t, f).call(this, e), n(this, t, _).call(this);
   }
   async enableAll() {
-    n(this, t, d).call(this), i(this, h, !0), n(this, t, g).call(this);
+    n(this, t, _).call(this), i(this, h, !0), n(this, t, g).call(this);
     for (let e = 1; e <= 90; e++)
       await this.enable(e), i(this, a, e), n(this, t, g).call(this);
-    i(this, a, 90), n(this, t, g).call(this), n(this, t, d).call(this);
+    i(this, a, 90), n(this, t, g).call(this), n(this, t, _).call(this);
   }
   async disableAll() {
-    n(this, t, d).call(this), i(this, h, !0), n(this, t, b).call(this);
+    n(this, t, _).call(this), i(this, h, !0), n(this, t, b).call(this);
     for (let e = 1; e <= 90; e++)
       await this.enable(e), i(this, a, e), n(this, t, b).call(this);
-    i(this, a, 90), n(this, t, b).call(this), n(this, t, d).call(this);
+    i(this, a, 90), n(this, t, b).call(this), n(this, t, _).call(this);
   }
 }
 h = new WeakMap(), a = new WeakMap(), o = new WeakMap(), t = new WeakSet(), y = function() {
   const e = ["percentage:disable", "percentage:enable", "percentage:open"];
   for (const s of e)
     this.serialRegisterAvailableListener(s);
-}, d = function() {
+}, _ = function() {
   i(this, h, !1), i(this, a, 0), i(this, o, 0);
 }, f = function(e = null) {
   i(this, o, Math.round(r(this, a) * 100 / 90)), this.dispatch("percentage:open", { percentage: r(this, o), dispensed: e });
