d09485dee2dd70fdb08e52f949ab795d61dced8f
danidoble <ddanidoble@gmail.com>
2025-11-18 12:43:53 -0600

    update tests arduino

--- test/connection.arduino.ts
+++ test/connection.arduino.ts
@@ -1,162 +1,118 @@
-import { Arduino } from './arduino';
-import { Devices } from '../lib/utils/devices';
+import { Arduino } from "./arduino.ts";
 
-Devices.registerType('arduino');
-
-// @ts-expect-error adding custom get functions
-Devices.getArduinoByUuid = (id: string) => {
-  return Devices.get('arduino', id);
-};
-
-// @ts-expect-error adding custom get functions
-Devices.getArduino = (device_number: number = 1) => {
-  return Devices.getByNumber('arduino', device_number);
-};
-
-const device = new Arduino();
-device.on('serial:message', ({ detail }: any) => {
-  console.log(detail);
+let logElement: HTMLElement | null = document.getElementById("log");
+document.addEventListener("DOMContentLoaded", (): void => {
+  if (!logElement) logElement = document.getElementById("log");
 });
 
-device.on('serial:timeout', ({ detail }: any) => {
-  console.log('serial:timeout', detail);
+const board = new Arduino({
+  // bypassSerialBytesConnection: true
+  // socket: true,
+});
+// board.portPath='/dev/ttyUSB0';
+
+// board.__debug__ = true;
+// board.on("debug", (event): void => {
+//   // @ts-expect-error detail is defined
+//   const detail = event.detail;
+//   console.log("debug", detail);
+// })
+
+// board.useRTSCTS = true; // Enable RTS/CTS flow control
+// board.fixedBytesMessage = 11;
+// board.timeoutBeforeResponseBytes = 50; // prev versions 400ms
+// board.responseDelimited = true;
+// board.responseLimiter = '\n';
+// board.responseSufixLimited = true;
+// board.responsePrefixLimited = true;
+
+board.on("serial:message", (data): void => {
+  // @ts-expect-error detail is defined
+  const detail = data.detail;
+  //console.log(detail);
+  if (!logElement) return;
+  logElement.innerText += 'Response of "' + detail.request + '": ' + detail.code.toString() + "\n\n";
 });
 
-// device.on('serial:sent', data => {
-//     console.log('serial:sent',data.detail);
-// });
-
-device.on('serial:error', ({ detail }: any) => {
-  const el = document.getElementById('log');
-  if (!el) return;
-  el.innerText += detail.message + '\n\n';
+board.on("serial:timeout", (data): void => {
+  // @ts-expect-error detail is defined
+  console.warn("serial:timeout", data.detail);
 });
 
-device.on('serial:disconnected', ({ target }: any) => {
-  const el = document.getElementById('log');
-  if (el) {
-    el.innerText += target.typeDevice + ' Disconnected\n\n';
-  }
-  const disconnectedEl = document.getElementById('disconnected');
-  const connectEl = document.getElementById('connect');
-  const disconnectEl = document.getElementById('disconnect');
-
-  if (disconnectedEl) {
-    disconnectedEl.classList.remove('hidden');
-  }
-  if (connectEl) {
-    connectEl.classList.remove('hidden');
-  }
-  if (disconnectEl) {
-    disconnectEl.classList.add('hidden');
-  }
+board.on("serial:sent", (data): void => {
+  // @ts-expect-error detail is defined
+  const detail = data.detail;
+  //console.log("serial:sent", detail);
+  if (!logElement) return;
+  logElement.innerText +=
+    'Action "' + detail.action + '": sent: ' + board.parseUint8ArrayToString(detail.bytes) + "\n\n";
 });
 
-device.on('serial:connecting', ({ detail }: any) => {
-  const connect = document.getElementById('connect');
-  if (connect && detail.active) {
-    connect.setAttribute('disabled', 'disabled');
-  } else if (connect) {
-    connect.removeAttribute('disabled');
-  }
-  const disconnect = document.getElementById('disconnect');
-  if (disconnect && detail.active) {
-    disconnect.setAttribute('disabled', 'disabled');
-  } else if (disconnect) {
-    disconnect.removeAttribute('disabled');
-  }
-
-  if (detail.active) return;
-  const el = document.getElementById('log');
-  if (el) {
-    el.innerText += 'Connecting finished\n\n';
-  }
+board.on("serial:error", (event): void => {
+  if (!logElement) return;
+  // @ts-expect-error detail is defined
+  logElement.innerText += event.detail.message + "\n\n";
 });
 
-device.on('serial:connected', ({ target }: any) => {
-  const el = document.getElementById('log');
-  if (el) {
-    el.innerText += target.typeDevice + ' Connected\n\n';
+board.on("serial:disconnected", (): void => {
+  // console.log(event);
+  if (logElement) {
+    logElement.innerText += "Disconnected\n\n";
   }
 
-  const disconnected = document.getElementById('disconnected');
-  if (disconnected) {
-    disconnected.classList.add('hidden');
-  }
-  const need = document.getElementById('need-permission');
-  if (need) {
-    need.classList.add('hidden');
-  }
-  const connect = document.getElementById('connect');
-  if (connect) {
-    connect.classList.add('hidden');
-  }
-  const disconnect = document.getElementById('disconnect');
-  if (disconnect) {
-    disconnect.classList.remove('hidden');
-  }
+  document.getElementById("disconnected")?.classList.remove("hidden");
+  document.getElementById("connect")?.classList.remove("hidden");
+  document.getElementById("disconnect")?.classList.add("hidden");
 });
 
-device.on('serial:need-permission', ({ target }: any) => {
-  const el = document.getElementById('log');
-  if (el) {
-    el.innerText += target.typeDevice + ' Need permissions to access serial devices\n\n';
-  }
-  const disconnected = document.getElementById('disconnected');
-  if (disconnected) {
-    disconnected.classList.remove('hidden');
-  }
-  const need = document.getElementById('need-permission');
-  if (need) {
-    need.classList.remove('hidden');
-  }
-  const connect = document.getElementById('connect');
-  if (connect) {
-    connect.classList.remove('hidden');
-  }
-  const disconnect = document.getElementById('disconnect');
-  if (disconnect) {
-    disconnect.classList.add('hidden');
-  }
+board.on("serial:connecting", (event: any): void => {
+  if (!logElement || event.detail.active) return;
+  logElement.innerText += "Connecting finished\n\n";
 });
 
-//device.on('serial:soft-reload', (event) => {
-// reset your variables
-//});
+board.on("serial:connected", (): void => {
+  if (logElement) {
+    logElement.innerText += "Connected\n\n";
+  }
 
-//device.on('serial:unsupported', (event) => {
-//  document.getElementById('unsupported').classList.remove('hidden');
-//});
+  document.getElementById("disconnected")?.classList.add("hidden");
+  document.getElementById("need-permission")?.classList.add("hidden");
+  document.getElementById("connect")?.classList.add("hidden");
+  document.getElementById("disconnect")?.classList.remove("hidden");
+});
 
-const tryConnect = () => {
-  if (device.isConnected) return;
+board.on("serial:need-permission", (): void => {
+  document.getElementById("disconnected")?.classList.remove("hidden");
+  document.getElementById("need-permission")?.classList.remove("hidden");
+  document.getElementById("connect")?.classList.remove("hidden");
+  document.getElementById("disconnect")?.classList.add("hidden");
+});
 
-  device
+board.on("serial:soft-reload", (): void => {
+  // reset your variables
+});
+
+board.on("serial:unsupported", (): void => {
+  document.getElementById("unsupported")?.classList.remove("hidden");
+});
+
+async function tryConnect(): Promise<void> {
+  board
     .connect()
-    .then(() => {})
+    .then((): void => {})
     .catch(console.error);
-};
+}
 
-const tryDisconnect = () => {
-  if (device.isDisconnected) return;
-
-  device
-    .disconnect()
-    .then(() => {})
-    .catch(console.error);
-};
-
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", (): void => {
   tryConnect();
-  const connect = document.getElementById('connect');
-  if (connect) {
-    connect.addEventListener('click', tryConnect);
-  }
-  const disconnect = document.getElementById('disconnect');
-  if (disconnect) {
-    disconnect.addEventListener('click', tryDisconnect);
-  }
+  document.getElementById("connect")?.addEventListener("click", tryConnect);
+  document.getElementById("disconnect")?.addEventListener("click", async () => {
+    await board.disconnect().catch(console.error);
+    if (!logElement) return;
+    logElement.innerText += "Disconnected by user\n\n";
+  });
 });
 
-// @ts-expect-error declare device in window
-window.device = device;
+// just to test in navigator
+// @ts-expect-error assign to window object
+window.device = board;
